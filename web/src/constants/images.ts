d7aa422af00619ada7c9fb87b9482ee71a41cc52
WilerMS <wilersoriano@gmail.com>
2024-03-21 00:04:45 +0100

    add constant folder for images and config

---
+++ web/src/constants/images.ts
@@ -0,0 +1,18 @@
+import gun1 from '../assets/players/color_1/Gun_01.png'
+import tank1 from '../assets/players/color_1/tank_1.png'
+
+import gun2 from '../assets/players/color_2/Gun_01.png'
+import tank2 from '../assets/players/color_2/tank_1.png'
+
+import gun3 from '../assets/players/color_2/Gun_01.png'
+import tank3 from '../assets/players/color_2/tank_1.png'
+
+import gun4 from '../assets/players/color_2/Gun_01.png'
+import tank4 from '../assets/players/color_2/tank_1.png'
+
+export const tankImages = [
+  { tank: tank1, gun: gun1 },
+  { tank: tank2, gun: gun2 },
+  { tank: tank3, gun: gun3 },
+  { tank: tank4, gun: gun4 },
+]
