eb014b7f2378bc10ae78db483a65bbdb195ab92c
WilerMS <wilersoriano@gmail.com>
2024-03-20 15:43:35 +0100

    add a controller class

---
+++ src/Controller.ts
@@ -0,0 +1,69 @@
+
+export type Keys = {
+  w: boolean,
+  s: boolean,
+  d: boolean,
+  a: boolean
+}
+
+export type Mouse = {
+  x: number,
+  y: number
+}
+
+export class Controller {
+
+  keys!: Keys
+  mouse!: Mouse
+
+  constructor() {
+
+    this.keys = {
+      w: false,
+      s: false,
+      d: false,
+      a: false
+    }
+
+    this.mouse = {
+      x: 0,
+      y: 0
+    }
+
+    document.addEventListener('keydown', event => {
+      const currentKey = event.key.toLowerCase() as keyof Keys
+      // If theres no control keys pressed, then finish
+      if (!Object.keys(this.keys).includes(currentKey)) return
+      this.keys[currentKey] = true
+
+      // Emit keyboard method
+      this.keyboard(this.keys)
+    })
+
+    document.addEventListener('keyup', event => {
+      const currentKey = event.key.toLowerCase() as keyof Keys
+      // If theres no control keys pressed, then finish
+      if (!Object.keys(this.keys).includes(currentKey)) return
+      this.keys[currentKey] = false
+
+      // Emit keyboard method
+      this.keyboard(this.keys)
+    })
+
+    document.addEventListener('mousemove', event => {
+      this.mouse.x = event.clientX
+      this.mouse.y = event.clientY
+      // this.mousemove(event.clientX, event.clientY)
+    })
+
+    document.addEventListener('click', (event) => {
+      this.click(event.clientX, event.clientY)
+    })
+
+  }
+
+  keyboard(keys: Keys) {}
+  mousemove(x: number, y: number) {}
+  click(x: number, y: number) {}
+
+}
